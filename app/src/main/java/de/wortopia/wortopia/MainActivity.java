e9ca2bdf12de7f2ad4194c5e0dc5848a46f9d376
Marek Ventur <marekventur@gmail.com>
2015-04-04 03:07:45 +0100

    Initial commit

---
+++ app/src/main/java/de/wortopia/wortopia/MainActivity.java
@@ -0,0 +1,25 @@
+package de.wortopia.wortopia;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://wortopia.de");
+        mWebView.setWebViewClient(new WebViewClient());
+    }
+}
